ff206281439ce5c57e318a8f9be254fa51671cb6
Sam Christy <sam_christy@hotmail.co.uk>
2012-06-03 20:37:09 +0200

    Update master

--- imagick/PieChart.php
+++ imagick/PieChart.php
@@ -1,10 +1,8 @@
+<?php
 /**
  * @author      Sam Christy <sam_christy@hotmail.co.uk>
  * @licence     GNU GPL v3.0 <http://www.gnu.org/licenses/gpl-3.0.html>
  * @copyright   Sam Christy 2012 | All rights reserved (c)
- */
-<?php
-/**
  * Super cool pie chart drawing class that uses ImageMagick because it's superior to GD!
  */
 class PieChart {
