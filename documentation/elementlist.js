1f9e6dabf6f458fab49dd8a76b0adb72e9578dca
Sam Christy <sam_christy@hotmail.co.uk>
2013-03-06 20:59:23 +0000

    Generate documentation with ApiGen
    ApiGen version 2.8.0 (http://apigen.org/)

---
+++ documentation/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","imageSmoothArc()"],["f","imageSmoothArcDrawSegment()"],["c","PieChart"],["c","PieChartColor"],["c","PieChartGD"],["c","PieChartImagick"]];
